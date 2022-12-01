2f5d9dd54925a7ada486be2527cdc82057a3b10b
kacper-reiman <117027512+kacper-reiman@users.noreply.github.com>
2022-12-01 18:55:58 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const ratingSection = document.querySelector(".rating");
+const thanksSection = document.querySelector(".thanks");
+const submitButton = document.getElementById("submit__btn");
+const goBack = document.querySelector(".attribution");
+const ratingButton = document.querySelectorAll(".rating__btn");
+const choosenRating = document.getElementById("rating__container");
+
+submitButton.addEventListener("click", () => {
+  thanksSection.classList.remove("hidden");
+  ratingSection.classList.add("hidden");
+});
+goBack.addEventListener("click", () => {
+  thanksSection.classList.add("hidden");
+  ratingSection.classList.remove("hidden");
+});
+ratingButton.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    choosenRating.innerHTML = rate.innerHTML;
+  });
+});
