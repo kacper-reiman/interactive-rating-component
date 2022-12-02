7fa271a1a347dd09f8f6fce2c0160291d452383a
kacper-reiman <117027512+kacper-reiman@users.noreply.github.com>
2022-12-02 13:53:13 +0100

    minor improvements
    added animation, improved script

--- main.js
+++ main.js
@@ -5,16 +5,30 @@ const goBack = document.querySelector(".attribution");
 const ratingButton = document.querySelectorAll(".rating__btn");
 const choosenRating = document.getElementById("rating__container");
 
-submitButton.addEventListener("click", () => {
-  thanksSection.classList.remove("hidden");
-  ratingSection.classList.add("hidden");
-});
-goBack.addEventListener("click", () => {
-  thanksSection.classList.add("hidden");
-  ratingSection.classList.remove("hidden");
-});
+// submitButton.addEventListener("click", () => {
+//   thanksSection.classList.remove("hidden");
+//   ratingSection.classList.add("hidden");
+// });
+// goBack.addEventListener("click", () => {
+//   thanksSection.classList.add("hidden");
+//   ratingSection.classList.remove("hidden");
+// });
+// ratingButton.forEach((rate) => {
+//   rate.addEventListener("click", () => {
+//     choosenRating.innerHTML = rate.innerHTML;
+//   });
+// });
+
 ratingButton.forEach((rate) => {
   rate.addEventListener("click", () => {
     choosenRating.innerHTML = rate.innerHTML;
+    submitButton.addEventListener("click", () => {
+      thanksSection.classList.remove("hidden");
+      ratingSection.classList.add("hidden");
+    });
+    goBack.addEventListener("click", () => {
+      thanksSection.classList.add("hidden");
+      ratingSection.classList.remove("hidden");
+    });
   });
 });
